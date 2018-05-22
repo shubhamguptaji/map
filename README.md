16c70ff52ca6cc3e9b2aad9c43e447131837bc0d
Shubham Gupta <shubham2604gupta@gmail.com>
2018-05-22 13:49:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Description
+
+This is my Full Stack Web Developer II nanodegree second project.
+In this project you can find nearby hotels, libraries and hospitals by searching the place.
+
+# APIs Used 
+
+- Foursquare API
+- Google Maps API
+- jQuery
+- KnockoutJS
+
+# Steps to Run
+
+- Clone or download the project.
+- Run index.html file.
+
+# Screenshot
+
+![Optional Text](../master/myFolder/image.png)
---
+++ js/script.js
@@ -0,0 +1,185 @@
+var map,marker;
+var markers = [];
+var type = ['Hotel', 'Hospital' , 'Library'];
+var infowindow,bounds,geocoder;
+var count=0;
+// function to getplaces at the specified location using the foursquare API
+function getPlaces(location, type) {
+	var foursquare_client_id = 'WX1F1S0PHLZB5KI5E2OJIH50AOHVZIIXBP0A3N4NX4WIPMYQ';
+	var foursquare_client_secret = 'D2Z04G3N4XVWDQJ2O5G5S0SNQCOG15HLDYGDG1IMAMBYJPL4';
+	console.log(location.lat);
+	for(var i=0;i<type.length;i++) {
+		var foursquare_url = "https://api.foursquare.com/v2/venues/explore?limit=5&range=5000&ll="+location.lat+","+location.lng+"&client_id="+foursquare_client_id+"&client_secret="+foursquare_client_secret+"&v=20180522&query="+type[i];
+		$.ajax({
+			url: foursquare_url,
+			method: 'GET',
+			dataType: 'json',
+			data: { 'place': type[i] },
+			success: function(result) {	
+				createMarkers(result);
+			}
+		}).fail(function() {
+			alert("Foursquare API was not able to find the places. Sorry :(");
+		});
+	}
+}
+
+// function to load the map 
+function initMap() {
+	var defaultLocation =  {
+			lat:  51.508530,lng: -0.076132
+		};
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: defaultLocation,
+		zoom: 13
+	});
+
+	infowindow = new google.maps.InfoWindow();
+	geocoder = new google.maps.Geocoder();
+	getPlaces(defaultLocation, type);
+
+}
+
+// this function creates markers at the specified locations
+function createMarkers(locations) {
+	var places = locations.response.groups[0].items;
+	for(var i=0;i<places.length;i++) {
+		var pos = {
+			lat: places[i].venue.location.lat,
+			lng: places[i].venue.location.lng
+		};
+		var address = places[i].venue.location.address;
+		marker = new google.maps.Marker({
+			position: pos,
+			title: places[i].venue.name,
+			address: address,
+			animation: google.maps.Animation.DROP,
+			category: places[i].venue.categories[0].name,
+			id: markers.length
+		});
+
+		markers.push(marker);
+
+		marker.addListener('click', function() {
+			populateInfoWindow(this, infowindow);
+		});
+	}
+	if (count == type.length - 1) {
+		mdl.fillMarkersList();
+		showAllMarkers(markers);
+	}
+  	if (count == type.length) count = 1;
+  	count++;
+	
+}
+// this function shows all the markers
+function showAllMarkers(markers) {
+	bounds = new google.maps.LatLngBounds();
+	for(var i=0;i<markers.length;i++) {
+		markers[i].setMap(map);
+		bounds.extend(markers[i].position);
+	}
+	map.fitBounds(bounds);
+}
+
+
+// this function adds content to the marker
+function populateInfoWindow(marker, infowindow) {
+	if(infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.addListener('closeclick', function() {
+			infowindow.setMarker = null;
+		});
+		infowindow.setContent('<div> <b>Title:</b>' + marker.title + '</div><div> <b>Address:</b>' + marker.address + '</div><div><b>Type:</b>' + marker.category + '</div>');
+		infowindow.open(map, marker);
+		
+	}
+}
+
+// this function finds the location searched by the user and then creates markers and displays them
+function findlocation(address) {
+	for(var i=0;i<markers.length;i++) {
+		markers[i].setMap(null);
+		console.log(i);
+	}
+	markers=[];
+	console.log(address);
+	if(address === '') {
+		alert("You must enter address first!!!!!!!");
+	} else {
+		geocoder.geocode({
+			'address': address,
+		}, function(results, status) {
+			if (status === google.maps.GeocoderStatus.OK) {
+				var loc = results[0].geometry.location.toString();
+      			var locarray = loc.split('(')[1].split(',');
+      			var latitude = parseFloat(locarray[0]);
+      			var longitude = parseFloat(locarray[1].split(')')[0]);
+      			var searchedLocation = {
+        			lat: latitude,
+        			lng: longitude
+        		};
+				map.setCenter(searchedLocation);
+				map.setZoom(13);
+				getPlaces(searchedLocation,type);
+			} else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        	}
+		});
+	}
+}
+
+// knockout model to store marker data
+var markerData = function(data) {
+	this.id = ko.observable(data.id);
+	this.title = ko.observable(data.title);
+	this.category = ko.observable(data.category);
+}
+// knockout viewmodel to load the content on the page
+var viewModel = function() {
+	var self = this;
+	self.defaultLoc = ko.observable("London");
+	self.categories = ko.observableArray(['Hotel', 'Hospital' , 'Library']);
+	self.markersList = ko.observableArray([]);
+	selectedCategory = ko.observable();
+//  set the locations enter by the user
+	self.start = function() {
+		findlocation(self.defaultLoc());
+	};
+	// fills the markers list
+	self.fillMarkersList = function(marker) {
+		self.markersList([]);
+		for(var i=0;i<markers.length;i++) {
+			self.markersList.push(new markerData(markers[i]));
+		}
+	};
+	// bounce the markers
+	self.animateMarker = function(a) {
+		for(var i=0;i<markers.length;i++) {
+			if(a.id() === markers[i].id ) {
+				for(var j=0;j<markers.length;j++) {
+					markers[j].setAnimation(null);
+				}
+				markers[i].setAnimation(google.maps.Animation.BOUNCE);
+				populateInfoWindow(markers[i], infowindow);
+				break;	
+			}
+		}
+	};
+	// filters the markers according to there category
+	self.filterMarkers = function() {
+		self.markersList([]);
+		for(var i=0;i<markers.length;i++) {
+			if(markers[i].category == selectedCategory() || selectedCategory() == undefined) {
+				markers[i].setVisible(true);
+				self.markersList.push(new markerData(markers[i]));
+			}
+			else {
+				markers[i].setVisible(false);
+			}
+		}
+	};
+}
+
+mdl = new viewModel();
+ko.applyBindings(mdl);
